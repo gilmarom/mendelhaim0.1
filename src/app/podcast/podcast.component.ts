0e449a074615138a7d8ed35c8204013d024b9675
gilmarom <gilmarom23@gmail.com>
2019-06-06 19:50:16 +0300

    Update podcast.component.ts

--- src/app/podcast/podcast.component.ts
+++ src/app/podcast/podcast.component.ts
@@ -18,7 +18,7 @@ export class PodcastComponent implements OnInit {
        this.podcasts = [
       {id:1, name: 'The lottery of life', particpate: 'Dr Anat Chesner-haviv'},
       {id:2, name:'Under Pressure',particpate: 'Dr Scott Sherr'},
-      {id:3, name:'Integrated Wisdom',particpate: ''}, 
+      {id:3, name:'Mission Alpha',particpate: 'Sergey Young'}, 
       {id:4, name:'Coming soon!',particpate: ''},
       {id:5, name:'Coming soon!',particpate: ''}]
   }

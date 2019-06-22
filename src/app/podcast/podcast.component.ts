6668654ba4bc4f6aed98e232f36c51004fde4db6
gilmarom <gilmarom23@gmail.com>
2019-06-22 16:31:05 +0300

    Update podcast.component.ts

--- src/app/podcast/podcast.component.ts
+++ src/app/podcast/podcast.component.ts
@@ -16,7 +16,7 @@ export class PodcastComponent implements OnInit {
 
   ngOnInit() {
        this.podcasts = [
-      {id:1, name: 'The lottery of life', particpate: 'Dr Anat Chesner-haviv'},
+      {id:1, name: 'The lottery of life', particpate: 'Dr Anat Chesner-Haviv'},
       {id:2, name:'Under Pressure',particpate: 'Dr Scott Sherr'},
       {id:3, name:'Mission Alpha',particpate: 'Sergey Young'}, 
       {id:4, name:'Heaven on Earth',particpate: 'James Strole'},
